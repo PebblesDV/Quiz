4df1e1364efac49c52c84f9134dc39c9e752d282
PebblesDV <pebblesdevries@gmail.com>
2023-10-05 11:03:55 +0200

    FinishedQuiz

--- sketch.js
+++ sketch.js
@@ -1,6 +1,8 @@
 let canvasWidth = 800;
 let canvasHeight = 600;
-let hasAlreadyAnswered = false;
+let hasAlreadyAnswered = false; //lets me check whether a question has already been answered
+let colorWhite = (255, 255, 255);
+let colorBlack = (0, 0, 0);
 
 //variables i use later to keep track of the score and the current question
 let currentQuestion = 0;
@@ -23,40 +25,40 @@ let quizData = [
     options: ["15 and under", "16-18", "19-23", "24-39"],
     rightAnswer: "19-23",
   },
-  // {
-  //   question:
-  //     "In order to be considered a good lift in competition, how many judges must give a white light?",
-  //   options: ["1", "2", "3", "4"],
-  //   rightAnswer: "2",
-  // },
-  // {
-  //   question:
-  //     "How many attemps to lift as heavy as you can do you get per category?",
-  //   options: ["2", "3", "4", "5"],
-  //   rightAnswer: "3",
-  // },
-  // {
-  //   question: "What is commonly used in competitions to improve grip?",
-  //   options: ["Lifting straps", "Chalk", "Baby powder", "Vaseline"],
-  //   rightAnswer: "Chalk",
-  // },
-  // {
-  //   question: "What is the main focus in powerlifting?",
-  //   options: ["Endurance", "Speed", "Strength", "Flexibility"],
-  //   rightAnswer: "Strength",
-  // },
-  // {
-  //   question:
-  //     "What is the weight of a standard barbell used in men's competitions?",
-  //   options: ["15kg", "20kg", "25kg", "30kg"],
-  //   rightAnswer: "20kg",
-  // },
-  // {
-  //   question:
-  //     "What piece of equipment is mandatory for a powerlifter to wear during official competitions?",
-  //   options: ["Belt", "Weightlifting gloves", "Knee sleeves", "Singlet"],
-  //   rightAnswer: "Singlet",
-  // },
+  {
+    question:
+      "In order to be considered a good lift in competition, how many judges must give a white light?",
+    options: ["1", "2", "3", "4"],
+    rightAnswer: "2",
+  },
+  {
+    question:
+      "How many attemps to lift as heavy as you can do you get per category?",
+    options: ["2", "3", "4", "5"],
+    rightAnswer: "3",
+  },
+  {
+    question: "What is commonly used in competitions to improve grip?",
+    options: ["Lifting straps", "Chalk", "Baby powder", "Vaseline"],
+    rightAnswer: "Chalk",
+  },
+  {
+    question: "What is the main focus in powerlifting?",
+    options: ["Endurance", "Speed", "Strength", "Flexibility"],
+    rightAnswer: "Strength",
+  },
+  {
+    question:
+      "What is the weight of a standard barbell used in men's competitions?",
+    options: ["15kg", "20kg", "25kg", "30kg"],
+    rightAnswer: "20kg",
+  },
+  {
+    question:
+      "What piece of equipment is mandatory for a powerlifter to wear during official competitions?",
+    options: ["Belt", "Weightlifting gloves", "Knee sleeves", "Singlet"],
+    rightAnswer: "Singlet",
+  },
 ];
 
 function setup() {
@@ -66,7 +68,7 @@ function setup() {
 }
 
 function draw() {
-  background(0, 0, 0); //black
+  background(colorBlack); //black
 
   fill("purple"); //decoration for the background
   stroke("pink");
@@ -74,26 +76,26 @@ function draw() {
   ellipse(10, 600, 500, 500);
   ellipse(800, 10, 700, 700);
 
-  fill("white");
-  stroke("black");
+  fill(colorWhite);
+  stroke(colorBlack);
   strokeWeight(2);
   textSize(20);
   textAlign(CENTER);
   text(score, 130, 500); //shows score on the screen
   text("Score:", 90, 500);
-  text(currentQuestion, 735, 40);
+  text(currentQuestion + 1, 735, 40);
   text("Question:", 680, 40); //shows question number on the screen
 
-  showQuestion(currentQuestion);
+  showQuestion(currentQuestion); //shows question on the screen
 }
 
 function showQuestion(questionId) {
-  const question = quizData[questionId];
+  const question = quizData[questionId]; //function to show questions and answer buttons
 
-  stroke("black");
+  stroke(colorBlack);
   strokeWeight(2);
   textSize(30);
-  fill(255, 255, 255); //white
+  fill(colorWhite); //white
   textWrap(WORD);
   textAlign(CENTER);
   text(question.question, 150, 80, 500); //showing question on the screen
@@ -109,13 +111,14 @@ function showQuestion(questionId) {
 }
 
 function handleClickAnswer(givenAnswer, rightAnswer) {
+  //function to show "Next question" button after an answer has been given
   if (hasAlreadyAnswered === false) {
     checkAnswer(givenAnswer, rightAnswer);
     button = createButton("Next question"); //button to continue to the next question
     button.position(600, 480);
     button.mousePressed(nextQuestion);
   }
-  hasAlreadyAnswered = true;
+  hasAlreadyAnswered = true; //checking if a question has already been answered
 }
 
 function checkAnswer(givenAnswer, rightAnswer) {
@@ -125,7 +128,7 @@ function checkAnswer(givenAnswer, rightAnswer) {
     textSize(20);
     fill(0, 255, 0); //green
     text("Answer is correct!", 645, 455); //Feedback on whether the answer was correct or wrong
-    score++;
+    score++; //adds +1 to the score after every right answer
   } else {
     correctAnswer = false;
     textSize(20);
@@ -135,29 +138,31 @@ function checkAnswer(givenAnswer, rightAnswer) {
 }
 
 function nextQuestion() {
+  //function on what to do after each question
   if (currentQuestion + 1 < quizData.length) {
     hasAlreadyAnswered = false;
     const allButtons = selectAll("button");
     for (let i = 0; i < allButtons.length; i++) {
-      allButtons[i].remove();
+      allButtons[i].remove(); //removes all buttons off the screen
     }
 
-    currentQuestion++;
-    clear();
-    redraw();
+    currentQuestion++; //adds +1 to question counter after every question
+    clear(); //clears the canvas
+    redraw(); //redraws everything
   } else {
     showResults();
   }
 }
 
 function showResults() {
+  //function on what to do after last question
   const allButtons = selectAll("button");
   for (let i = 0; i < allButtons.length; i++) {
-    allButtons[i].remove();
+    allButtons[i].remove(); //removes all buttons off the screen
   }
-  clear();
+  clear(); //clears the canvas
 
-  background(0, 0, 0); //black
+  background(colorBlack); //black
 
   fill("purple"); //decoration for the background
   stroke("pink");
@@ -165,11 +170,11 @@ function showResults() {
   ellipse(10, 600, 500, 500);
   ellipse(800, 10, 700, 700);
 
-  fill("white");
-  stroke("black");
+  fill(colorWhite);
+  stroke(colorBlack);
   strokeWeight(2);
   textSize(40);
-  text("Your final score is:", 225, 190);
+  text("Your final score is:", 225, 190); //shows final score when quiz is done
   text(score, 225, 260);
 
   textSize(15);
